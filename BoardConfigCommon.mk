0f4ee6a46e05d506869100dcb1cfbf5813e8906c
Raj Jadeja <rajsjadeja4@gmail.com>
2020-09-12 08:00:31 +0000

    sdm845-common: temp Fake sepolicy

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -124,16 +124,17 @@ BOARD_ROOT_EXTRA_FOLDERS := op1 op2 op_odm
 TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 
 # Sepolicy
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
-BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
+SELINUX_IGNORE_NEVERALLOWS := true
+#BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/fake-sepolicy/private
 
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/qcom/sepolicy/generic/private \
-    device/qcom/sepolicy/qva/private
+#BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+#    device/qcom/sepolicy/generic/private \
+#    device/qcom/sepolicy/qva/private
 
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
-    device/qcom/sepolicy/generic/public \
-    device/qcom/sepolicy/qva/public
+#BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+#    device/qcom/sepolicy/generic/public \
+#    device/qcom/sepolicy/qva/public
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
