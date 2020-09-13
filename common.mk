69f9fca288017c16c2c01c35133f8cda4adf8b2b
Jyotiraditya <imjyotiraditya@pm.me>
2020-09-13 06:04:58 +0000

    sdm845-common: build libhwbinder and copy some prebuilts from vndk29
    Test: build, ril
    Signed-off-by: Jyotiraditya <imjyotiraditya@pm.me>

--- common.mk
+++ common.mk
@@ -93,6 +93,16 @@ PRODUCT_PACKAGES_DEBUG += \
 PRODUCT_PACKAGES += \
     Snap
 
+# Binder
+PRODUCT_PACKAGES += \
+    libhwbinder \
+    libhwbinder.vendor \
+    libhidltransport
+
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full.so \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite.so
+
 # Common init scripts
 PRODUCT_PACKAGES += \
     init.qcom.rc \
